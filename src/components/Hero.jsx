e986c2919258468750dee8b9a151f9a14295a8ff
JakAsAlways <mrjak366@gmail.com>
2024-08-16 16:46:12 +0400

    finished the frontend

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from './Button'
+
+export default function Hero() {
+    return (
+        <div className='min-h-screen flex flex-col gap-10 items-center 
+        justify-center text-center max-w-[800px] w-full mx-auto p-4'>
+            <p>IT'S TIME TO GET</p>
+            <div className="flex flex-colum">
+
+                <h1 className='uppercase font-semibold text-4xl sm:text-5xl
+       md:text-6xl large:text-8xl'>Swole
+                    <span className='text-blue-400'>normous</span> </h1>
+            </div>
+            <p className='text-sm medium:text-vase font-light'>I hereby acknowledgement that I may become
+                <span className='text-blue-400 font-medium'>unbelievably swolenormous</span>
+                and accept all risks of becoming the local
+                <span className='text-blue-400 font-medium'>mass montrosity</span>
+                , afflicted with severe body dismorphia,
+                unable to fit through doors.</p>
+            <Button func={() => {
+                window.location.href = '#generate'
+            }} text={"Accept and Begin"}/>
+        </div>
+    )
+}
